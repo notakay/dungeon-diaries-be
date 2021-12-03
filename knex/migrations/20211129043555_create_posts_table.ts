a25c03ae9a15906dbaf711f89eaf82bc8dfffa78
akay <aungkhantko@protonmail.com>
2021-12-02 20:07:24 -0800

    Changed description field to string with limit 1000 chars

--- knex/migrations/20211129043555_create_posts_table.ts
+++ knex/migrations/20211129043555_create_posts_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('posts', (table) => {
     table.increments('id').primary();
     table.string('title').notNullable();
-    table.string('description').notNullable();
+    table.string('content', 1000).notNullable();
     table.integer('votes').defaultTo(0);
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
