4a09a77ced5009972dd187cd8cf6ff58b15dea41
aungpkhant <derrick.aungkhant@gmail.com>
2021-12-04 00:55:55 +0630

    Dot env configuration

--- knexfile.ts
+++ knexfile.ts
@@ -1,11 +1,12 @@
 import { Knex } from 'knex';
+import config from './config';
 
 const development: Knex.Config = {
   client: 'pg',
   connection: {
     host: '127.0.0.1',
     user: 'postgres',
-    password: 'postgres',
+    password: config['databasePassword'],
     database: 'dungeon_diaries',
     charset: 'utf8'
   },
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import config from '../config';
 import express, { Application } from 'express';
 import { apiRouter } from './api';
 import sessions from './middleware/sessions';
@@ -9,4 +10,4 @@ app.use(sessions);
 app.get('/', (_req, res) => res.send('hello'));
 app.use('/api', apiRouter);
 
-app.listen(3000, () => console.log('server running'));
+app.listen(config.port, () => console.log('server running'));
