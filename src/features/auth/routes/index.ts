e2f498640ff218f5dd77112a90a3881d896c5c17
akay <aungkhantko@protonmail.com>
2021-12-05 00:03:14 -0800

    Added validate middleware (#4)
    * Added validate middleware
    
    * Typescript props and adding schemas
    
    * Folder restructure and cleanup
    
    * Migrate to celebrate WIP
    
    Co-authored-by: aungpkhant <derrick.aungkhant@gmail.com>

---
+++ src/features/auth/routes/index.ts
@@ -0,0 +1,93 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import { celebrate } from 'celebrate';
+
+import knex from '../../../../knex/knex';
+import * as dbHelper from '../../../utils/db/helpers';
+import { BadRequestError } from '../../../utils/errors';
+
+import { registerSchema, loginSchema } from '../schemas/schemas';
+
+const authRouter: Router = Router();
+
+authRouter.post(
+  '/register',
+  celebrate(registerSchema),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { username, email, password } = req.body;
+
+      const usernameExists: boolean = await dbHelper.existsInTable(
+        'users',
+        'username',
+        username
+      );
+
+      if (usernameExists) {
+        throw new BadRequestError('Username is already taken');
+      }
+
+      const emailExists: boolean = await dbHelper.existsInTable(
+        'users',
+        'email',
+        email
+      );
+      if (emailExists) {
+        throw new BadRequestError('Email is already taken');
+      }
+
+      const hash: string = await bcrypt.hash(password, 10);
+
+      const id: Array<number> = await knex('users')
+        .insert({ username, email, password_hash: hash })
+        .returning('id');
+
+      res.send(`Successfully created user with user id ${id}`);
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+authRouter.post(
+  '/login',
+  celebrate(loginSchema),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body;
+
+      const record = await knex('users')
+        .select('id', 'username', 'email', 'password_hash')
+        .where('email', email)
+        .first();
+
+      if (!record) throw new BadRequestError('User with email does not exist');
+
+      const isValidPassword = await bcrypt.compare(
+        password,
+        record.password_hash
+      );
+
+      if (!isValidPassword) throw new BadRequestError('Invalid password');
+      req.session.user = { userId: record.id, isLoggedIn: true };
+
+      res.send('Success');
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+authRouter.post(
+  '/logout',
+  (req: Request, res: Response, next: NextFunction) => {
+    req.session.destroy((err) => {
+      if (err) {
+        next(err);
+      }
+      res.status(200).send();
+    });
+  }
+);
+
+export { authRouter };
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { UnauthorizedError } from '../utils/errors';
 
-export const isLoggedIn = (req: Request, res: Response, next: () => void) => {
+export const isLoggedIn = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
   if (!req.session.user?.isLoggedIn || !req.session.user?.userId)
     throw new UnauthorizedError('User not logged in');
   next();
