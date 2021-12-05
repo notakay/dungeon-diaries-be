67199be68ca2724ab6184494e8474e6c7561c787
akay <aungkhantko@protonmail.com>
2021-12-04 17:03:11 -0800

    Added migration for creating comments

---
+++ knex/migrations/20211204213022_create_comments_table.ts
@@ -0,0 +1,27 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('comments', (table) => {
+    table.increments('id').primary();
+    table.integer('parent_id').defaultTo(null);
+    table.string('comment', 1000).notNullable();
+    table.integer('depth').defaultTo(0);
+    table.string('lineage', 1000).defaultTo('/');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('SET NULL');
+    table
+      .integer('post_id')
+      .references('id')
+      .inTable('posts')
+      .onDelete('SET NULL');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('comments');
+}
