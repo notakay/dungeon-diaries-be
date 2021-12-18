2a962c59694a95bc485fc7b10f8127c948fcc0c8
akay <aungkhantko@protonmail.com>
2021-12-18 00:31:24 -0800

    Fix migration files

--- knex/migrations/20211218065046_create_upload_intents_table.ts
+++ knex/migrations/20211218065046_create_upload_intents_table.ts
@@ -10,5 +10,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('comments');
+  return knex.schema.dropTable('upload_intents');
 }
