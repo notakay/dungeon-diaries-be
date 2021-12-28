28bb0a36956cd6ce0e72df64c1b366898f00d72d
akay <aungkhantko@protonmail.com>
2021-12-28 10:16:21 -0800

    Switching to redis for session maangement (#17)

--- config/index.ts
+++ config/index.ts
@@ -13,8 +13,11 @@ if (envFound.error && process.env.NODE_ENV !== 'docker') {
 export default {
   environment: process.env.NODE_ENV,
   port: parseInt(process.env.PORT! ?? 3000, 10),
-  databasePassword: process.env.POSTGRES_PASSWORD,
   sessionSecret: process.env.SESSION_SECRET ?? '',
   region: process.env.AWS_REGION,
-  bucket: process.env.AWS_BUCKET
+  bucket: process.env.AWS_BUCKET,
+  dbHost: process.env.DB_HOST ?? '127.0.0.1',
+  dbPassword: process.env.POSTGRES_PASSWORD,
+  redisHost: process.env.REDIS_HOST ?? '127.0.0.1',
+  redisPassword: process.env.REDIS_PASSWORD
 };
